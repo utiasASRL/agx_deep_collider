9d7f4fb87b79a01be80efc768fb09960792a3750
HuguesTHOMAS <huguesthomas218@gmail.com>
2021-11-09 08:23:25 -0500

    .

--- build_everything.sh
+++ build_everything.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 echo ""
-echo "Step 1: Build the catkin_ws"
 echo ""
+echo "0-----------------------------0"
+echo "| Step 1: Build the catkin_ws |"
+echo "0-----------------------------0"
 
 ./run_inference.sh -c "./build_catkin.sh"
 
 echo ""
-echo "Step 2: Build everything except ros1_bridge"
 echo ""
+echo "0---------------------------------------------0"
+echo "| Step 2: Build everything except ros1_bridge |"
+echo "0---------------------------------------------0"
 
 ./run_inference.sh -c "./build_elo_ws.sh"
 
 echo ""
-echo "Step 3: Build everything except ros1_bridge"
 echo ""
+echo "0---------------------------------------------0"
+echo "| Step 3: Build everything except ros1_bridge |"
+echo "0---------------------------------------------0"
 
 ./run_inference.sh -c "./build_ros1_bridge.sh"
